892b63d52ec0859fab6d64bdb257cd5e87517e8b
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-05 14:31:09 +0200

    questionbox

--- src/components/Questionbox.js
+++ src/components/Questionbox.js
@@ -1,2 +1,11 @@
 import React from 'react';
 
+const Questionbox = ({ question, options }) => {
+    return (
+        <div className="questionBox">
+            <div className="question">{question}</div>
+        </div>
+    )
+}
+
+export default Questionbox;
